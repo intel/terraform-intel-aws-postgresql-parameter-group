a4df3d78eedf0619f4a69c0d658c80fbdf04c3bb
JoshHilliker <josh.hilliker@intel.com>
2023-06-19 18:21:15 -0700

    more updates.

--- examples/create-parameters/main.tf
+++ examples/create-parameters/main.tf
@@ -3,7 +3,7 @@
 # Provision Intel Optimized AWS MSSQL server
 module "aws-postgresql-parameter-group" {
 #  source = "intel/aws-postgresql-paramter-group/intel"
-  source = "../../"
+  source = "/../.."
 }
 
 
--- examples/create-parameters/variables.tf
+++ examples/create-parameters/variables.tf
@@ -1,7 +1,11 @@
 ########################
 ####     Intel      ####
 ########################
-
+variable "region" {
+  description = "Target AWS region to deploy workloads in."
+  type        = string
+  default     = "us-east-1"
+}
 ########################
 ####    Required    ####
 ########################
