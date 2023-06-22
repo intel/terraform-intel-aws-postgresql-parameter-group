8cd94aa4a69c170f134c8349a219d2c92feec587
JoshHilliker <josh.hilliker@intel.com>
2023-06-22 09:24:01 -0700

    updates

--- examples/existing-module/main.tf
+++ examples/existing-module/main.tf
@@ -2,24 +2,37 @@
 
 module "aws-postgresql-parameter-group" {
 #  source = "intel/terraform-intel-aws-postgresql-parameter/intel"
-  source = "../../"
+  source = "../.."
 }
 
 # Example of how to pass variable for database password:
 # terraform apply -var="db_password=..."
-
+/*
 resource "aws_db_instance" "example" {
   identifier                = "my-postgresql-db"
   engine                    = "postgres"
   engine_version            = "14.7"
-  instance_class            = "db.m5i.large"
+  instance_class            = "db.m6i.large"
   allocated_storage         = 20
-  storage_type              = "gp2"
+  storage_type              = "io1"
   publicly_accessible       = false
   db_subnet_group_name      = aws_db_subnet_group.example.name
   vpc_security_group_ids    = [aws_security_group.example.id]
   name                      = "testpostgresql"
   username                  = "myusername"
-  password                  = "var.db_password"
-  parameter_group_name      = "default.postgres13"
+  password                  = var.db_password
+  parameter_group_name      = module.aws-postgresql-parameter-group.db_parameter_group_name
+} */
+
+resource "aws_db_instance" "postgresql" {
+  allocated_storage    = 10
+  db_name              = "testpostgresqlrds"
+  identifier           = "postgresql-intel-optimized"
+  engine               = "postgres"
+  engine_version       = "14.7"
+  instance_class       = "db.m6i.large"
+  username             = "admin25"
+  password             = "var.db_password"
+  parameter_group_name = module.aws-postgresql-parameter-group.db_parameter_group_name
+  skip_final_snapshot  = true
 }
